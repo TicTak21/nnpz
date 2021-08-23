c9d3e20da5b16f705461f47112dc8f40cc0d691b
Alex <tictak211@gmail.com>
2021-08-23 23:58:57 +0300

    feat(server): configure env

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { cwd } from 'process';
 import { GqlModule } from './modules/gql/gql.module';
 import { PizzaModule } from './modules/pizza/pizza.module';
 
 @Module({
-  imports: [PizzaModule, GqlModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `${cwd()}/config/.env`, isGlobal: true }),
+    PizzaModule,
+    GqlModule,
+  ],
 })
 export class AppModule {}
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,4 +1,5 @@
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
@@ -6,11 +7,12 @@ import { PizzaEntity } from './app/modules/pizza/entities/pizza.entity';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 4000;
 
-  const config = new DocumentBuilder().setTitle('Nest Pizza API').setVersion('1.0').build();
+  const configService = app.get(ConfigService);
+  const port = configService.get('SERVER_PORT');
 
-  const document = SwaggerModule.createDocument(app, config, { extraModels: [PizzaEntity] });
+  const swaggerConfig = new DocumentBuilder().setTitle('Nest Pizza API').setVersion('1.0').build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig, { extraModels: [PizzaEntity] });
   SwaggerModule.setup('swagger', app, document);
 
   await app.listen(port, () => {
