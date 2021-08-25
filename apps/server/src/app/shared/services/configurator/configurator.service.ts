84ed1990dbd842d7fcb42953cf119efcfa8aec5e
Alex <tictak211@gmail.com>
2021-08-25 15:31:25 +0300

    refactor(server): bootstrap logic refactoring

---
+++ apps/server/src/app/shared/services/configurator/configurator.service.ts
@@ -0,0 +1,77 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
+import { from, Observable } from 'rxjs';
+import { PizzaEntity } from '../../../modules/pizza/entities/pizza.entity';
+import { ToppingEntity } from '../../../modules/topping/entities/topping.entity';
+
+/**
+ * Class that configures app. Works like `Builder`.
+ */
+export class Configurator {
+  private readonly configService: ConfigService;
+  public readonly port: number;
+
+  /**
+   * Creates an app.
+   * @param {NestExpressApplication} app - Nest application.
+   */
+  constructor(private readonly app: NestExpressApplication) {
+    this.configService = app.get(ConfigService);
+
+    // reusable variables(port, host, etc.)
+    this.port = this.configService.get('SERVER_PORT');
+  }
+
+  /**
+   * Since NestJS handles Observables automatically, there is no need to subscribe to returned stream.
+   *
+   * @example
+   * new Configurator(app).run();
+   *
+   * @returns Observable<void>
+   */
+  run(): Observable<void> {
+    this.addStaticAssets().addViews().addSwagger();
+
+    return from(this.app.listen(this.port, this.logListen));
+  }
+
+  private addSwagger(): this {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('Nest Pizza API')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(this.app, swaggerConfig, {
+      extraModels: [PizzaEntity, ToppingEntity],
+    });
+    SwaggerModule.setup('swagger', this.app, document);
+
+    return this;
+  }
+
+  private addMiddlewares() {
+    // app.use(...)
+
+    return this;
+  }
+
+  private addViews(): this {
+    this.app.setBaseViewsDir(join(__dirname, 'assets/views'));
+    this.app.setViewEngine('pug');
+
+    return this;
+  }
+
+  private addStaticAssets(): this {
+    this.app.useStaticAssets(join(__dirname, 'assets'));
+
+    return this;
+  }
+
+  private logListen() {
+    Logger.log('Listening at http://localhost:' + this.port);
+  }
+}
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,32 +1,9 @@
-import { Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { join } from 'path';
+import { from } from 'rxjs';
 import { AppModule } from './app/app.module';
-import { PizzaEntity } from './app/modules/pizza/entities/pizza.entity';
-import { ToppingEntity } from './app/modules/topping/entities/topping.entity';
+import { Configurator } from './app/shared/services/configurator/configurator.service';
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  const configService = app.get(ConfigService);
-  const port = configService.get('SERVER_PORT');
-
-  app.useStaticAssets(join(__dirname, 'assets'));
-  app.setBaseViewsDir(join(__dirname, 'assets/views'));
-  app.setViewEngine('pug');
-
-  const swaggerConfig = new DocumentBuilder().setTitle('Nest Pizza API').setVersion('1.0').build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig, {
-    extraModels: [PizzaEntity, ToppingEntity],
-  });
-  SwaggerModule.setup('swagger', app, document);
-
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port);
-  });
-}
-
-bootstrap();
+from(NestFactory.create<NestExpressApplication>(AppModule)).subscribe(app =>
+  new Configurator(app).run(),
+);
