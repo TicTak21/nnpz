365a058c0ec07d973dddcf76220a39a9bca87bda
Alex <tictak211@gmail.com>
2021-08-31 18:06:56 +0300

    feat(server): http exception filter

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER } from '@nestjs/core';
 import { cwd } from 'process';
 import { AppController } from './controllers/app.controller';
 import { DbModule } from './modules/db/db.module';
 import { GqlModule } from './modules/gql/gql.module';
 import { PizzaModule } from './modules/pizza/pizza.module';
 import { ToppingModule } from './modules/topping/topping.module';
-import { GqlExceptionFilter } from './shared/filters/gql/gql-exception.filter';
+import { globalFilters } from './shared/filters';
 
 @Module({
   imports: [
@@ -21,6 +20,6 @@ import { GqlExceptionFilter } from './shared/filters/gql/gql-exception.filter';
     ToppingModule,
   ],
   controllers: [AppController],
-  providers: [{ provide: APP_FILTER, useClass: GqlExceptionFilter }],
+  providers: [...globalFilters],
 })
 export class AppModule {}
---
+++ apps/server/src/app/shared/filters/http/http-exception.filter.ts
@@ -0,0 +1,35 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  Logger,
+} from '@nestjs/common';
+import { HttpArgumentsHost } from '@nestjs/common/interfaces';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter<T extends HttpException = HttpException>
+  implements ExceptionFilter
+{
+  catch(exception: T, host: ArgumentsHost) {
+    const filterName = this.constructor.name;
+
+    const ctx: HttpArgumentsHost = host.switchToHttp();
+    const { url, method }: Request = ctx.getRequest<Request>();
+    const res: Response = ctx.getResponse<Response>();
+    const status = exception.getStatus ? exception.getStatus() : 404;
+
+    const errorResponse = {
+      code: status,
+      timestamp: new Date().toISOString(),
+      path: url,
+      method: method,
+      message: exception.message,
+    };
+
+    Logger.error(`${method} ${url}`, JSON.stringify(errorResponse), filterName);
+
+    res.status(status).json(errorResponse);
+  }
+}
