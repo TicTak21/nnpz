f53202ca055a90a7d3a433ee2d6ec17512f0ed9c
Alex <tictak211@gmail.com>
2021-09-11 21:46:13 +0300

    feat(admin): base setup

---
+++ apps/admin/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { lyl, StyleRenderer, ThemeRef, ThemeVariables } from '@alyle/ui';
+import { Component } from '@angular/core';
+
+const STYLES = (theme: ThemeVariables, ref: ThemeRef) => {
+  ref.selectorsOf(STYLES);
+  return {
+    $global: lyl`{
+      body {
+        background-color: ${theme.background.default}
+        color: ${theme.text.default}
+        font-family: ${theme.typography.fontFamily}
+        margin: 0
+        direction: ${theme.direction}
+      }
+    }`,
+    root: lyl`{
+      display: block
+    }`,
+  };
+};
+
+@Component({
+  selector: 'client-root',
+  templateUrl: './app.component.html',
+  providers: [StyleRenderer],
+})
+export class AppComponent {
+  readonly classes = this.sRenderer.renderSheet(STYLES, true);
+
+  constructor(readonly sRenderer: StyleRenderer) {}
+}
