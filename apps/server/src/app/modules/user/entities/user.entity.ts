aec2d3fe0b53b76e9a588996fd6258d750bfb348
Alex <tictak211@gmail.com>
2021-09-16 20:26:18 +0300

    refactor(server): update entities using inheritance

--- apps/server/src/app/modules/user/entities/user.entity.ts
+++ apps/server/src/app/modules/user/entities/user.entity.ts
@@ -1,27 +1,17 @@
 import { EUserRole, IUserEntity } from '@nest-ng-pizza/types';
 import {
   Field,
-  ID,
   InputType,
   ObjectType,
   registerEnumType,
 } from '@nestjs/graphql';
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from '../../../shared/entities/base.entity';
 
 @Entity({ name: 'user' })
 @ObjectType('User')
 @InputType('UserInput')
-export class UserEntity implements IUserEntity {
-  @PrimaryGeneratedColumn('uuid')
-  @Field(_type => ID)
-  id: string;
-
+export class UserEntity extends BaseEntity implements IUserEntity {
   @Column({ type: 'varchar', length: 100 })
   @Field()
   email: string;
@@ -29,21 +19,6 @@ export class UserEntity implements IUserEntity {
   @Column({ type: 'varchar' })
   @Field()
   password: string;
-
-  @CreateDateColumn({
-    type: 'timestamptz',
-    default: () => 'now()',
-  })
-  @Field(_type => Date)
-  createdAt: string;
-
-  @UpdateDateColumn({
-    type: 'timestamptz',
-    default: () => 'now()',
-    onUpdate: 'now()',
-  })
-  @Field(_type => Date)
-  updatedAt: string;
 
   @Column({ type: 'enum', enum: EUserRole, default: EUserRole.client })
   @Field(_type => EUserRole, { defaultValue: EUserRole.client })
---
+++ apps/server/src/app/shared/entities/base.entity.ts
@@ -0,0 +1,29 @@
+import { IBaseEntity } from '@nest-ng-pizza/types';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@ObjectType({ isAbstract: true })
+export abstract class BaseEntity implements IBaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(_type => ID)
+  id: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'now()',
+  })
+  @Field(_type => Date)
+  createdAt: string;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'now()',
+    onUpdate: 'now()',
+  })
+  @Field(_type => Date)
+  updatedAt: string;
+}
