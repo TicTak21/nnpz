473c3777973efe9ac35bc65f566fc4318199612d
Alex <tictak211@gmail.com>
2021-09-24 19:44:19 +0300

    refactor(server): update `HashService` logic

--- apps/server/src/app/modules/auth/authentication/services/authentication.service.ts
+++ apps/server/src/app/modules/auth/authentication/services/authentication.service.ts
@@ -1,6 +1,6 @@
 import { EUserRole } from '@nest-ng-pizza/types';
 import { Injectable } from '@nestjs/common';
-import { Observable, of } from 'rxjs';
+import { Observable, of, switchMap } from 'rxjs';
 import { HashService } from '../../../../shared/services/crypto/hash.service';
 import { UserEntity } from '../../../user/entities/user.entity';
 import { UserService } from '../../../user/services/user.service';
@@ -22,12 +22,14 @@ export class AuthenticationService {
   }
 
   register({ email, password }: RegisterDto): Observable<UserEntity> {
-    const passwordHash = this.hashService.hash(password);
-
-    return this.userService.create({
-      email,
-      password: passwordHash,
-      role: EUserRole.client,
-    });
+    return this.hashService.hash(password).pipe(
+      switchMap(passwordHash =>
+        this.userService.create({
+          email,
+          password: passwordHash,
+          role: EUserRole.client,
+        }),
+      ),
+    );
   }
 }
--- apps/server/src/app/shared/services/crypto/hash.service.ts
+++ apps/server/src/app/shared/services/crypto/hash.service.ts
@@ -1,17 +1,45 @@
 import { Injectable } from '@nestjs/common';
-import { pbkdf2Sync, randomBytes } from 'crypto';
+import { Buffer } from 'buffer';
+import {
+  BinaryLike,
+  randomBytes,
+  scrypt as cryptoScript,
+  ScryptOptions,
+  timingSafeEqual,
+} from 'crypto';
+import { bindNodeCallback, map, Observable } from 'rxjs';
+
+const scrypt: (
+  password: BinaryLike,
+  salt: BinaryLike,
+  keylen: number,
+  options?: ScryptOptions,
+) => Observable<Buffer> = bindNodeCallback(cryptoScript);
 
 @Injectable()
 export class HashService {
-  hash(str: string): string {
-    const salt = randomBytes(16).toString('hex');
+  private readonly separator: string = ':';
+  private readonly keylen: number = 64;
+  private readonly encoding: BufferEncoding = 'hex';
+  private readonly bytesLength: number = 8;
 
-    return pbkdf2Sync(str, salt, 1000, 64, 'sha512').toString('hex');
+  hash(str: string): Observable<string> {
+    const salt = randomBytes(this.bytesLength).toString(this.encoding);
+
+    return scrypt(str, salt, this.keylen).pipe(
+      map(
+        derivedKey =>
+          salt + this.separator + derivedKey.toString(this.encoding),
+      ),
+    );
   }
 
-  compare(str: string, hash: string): boolean {
-    const hashedStr = this.hash(str);
+  compare(str: string, hash: string): Observable<boolean> {
+    const [salt, key] = hash.split(this.separator);
+    const keyBuffer = Buffer.from(key, this.encoding);
 
-    return hashedStr === hash;
+    return scrypt(str, salt, this.keylen).pipe(
+      map(derivedKey => timingSafeEqual(keyBuffer, derivedKey)),
+    );
   }
 }
