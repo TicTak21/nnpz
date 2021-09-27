f070a03cfe45b8bdb9eb995206c411039910c31a
Alex <tictak211@gmail.com>
2021-09-27 23:06:45 +0300

    feat(server): login with creds

--- apps/server/src/app/modules/auth/authentication/services/authentication.service.ts
+++ apps/server/src/app/modules/auth/authentication/services/authentication.service.ts
@@ -1,6 +1,15 @@
 import { EUserRole } from '@nest-ng-pizza/types';
-import { Injectable } from '@nestjs/common';
-import { Observable, of, switchMap } from 'rxjs';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import {
+  catchError,
+  mergeMap,
+  Observable,
+  of,
+  switchMap,
+  throwError,
+} from 'rxjs';
+import { ErrorHandler, errorHandlers } from '../../../../shared/error';
 import { CryptoService } from '../../../../shared/services/crypto/crypto.service';
 import { UserEntity } from '../../../user/entities/user.entity';
 import { UserService } from '../../../user/services/user.service';
@@ -11,10 +20,27 @@ export class AuthenticationService {
   constructor(
     private readonly userService: UserService,
     private readonly cryptoService: CryptoService,
+    private readonly jwtService: JwtService,
   ) {}
 
-  login(credentials: LoginDto): Observable<UserEntity> {
-    return of({} as UserEntity);
+  login({ email, password }: LoginDto): Observable<UserEntity> {
+    return this.userService.getByEmail(email).pipe(
+      mergeMap(user =>
+        this.cryptoService.compare(password, user.password).pipe(
+          mergeMap(validPass =>
+            validPass
+              ? this.signUser(user)
+              : throwError(() => ({ code: HttpStatus.BAD_REQUEST })),
+          ),
+          catchError(err => {
+            const errorHandler: ErrorHandler =
+              errorHandlers[err.code] ||
+              errorHandlers[HttpStatus.INTERNAL_SERVER_ERROR];
+            return throwError(errorHandler);
+          }),
+        ),
+      ),
+    );
   }
 
   logout(credentials: LogoutDto): Observable<UserEntity> {
@@ -31,5 +57,12 @@ export class AuthenticationService {
         }),
       ),
     );
+  }
+
+  private signUser(user: UserEntity): Observable<UserEntity> {
+    const payload = { id: user.id, email: user.email };
+    const token = this.jwtService.sign(payload);
+
+    return this.userService.update(user.id, { ...user, token });
   }
 }
