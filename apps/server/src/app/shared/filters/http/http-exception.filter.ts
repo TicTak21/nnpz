140ff55c0ca648fe2cb3a61a94569ead33599c84
Alex <tictak211@gmail.com>
2021-10-11 18:03:01 +0300

    feat(server): create global error filter(http) #4

--- apps/server/src/app/shared/filters/http/http-exception.filter.ts
+++ apps/server/src/app/shared/filters/http/http-exception.filter.ts
@@ -3,17 +3,36 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  HttpStatus,
+  Logger,
 } from '@nestjs/common';
-import { BaseExceptionFilter } from '@nestjs/core';
+import { Request, Response } from 'express';
 
 @Catch()
-export class HttpExceptionFilter
-  extends BaseExceptionFilter
-  implements ExceptionFilter
-{
+export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     if (host.getType() !== 'http') return;
 
-    super.catch(exception, host);
+    const ctx = host.switchToHttp();
+    const res = ctx.getResponse<Response>();
+    const { method, url } = ctx.getRequest<Request>();
+    const status = exception.getStatus
+      ? exception.getStatus()
+      : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const errorResponse = {
+      statusCode: status,
+      message: exception.message,
+      timestamp: new Date().toISOString(),
+      path: url,
+    };
+
+    Logger.error(
+      `${method}: ${url}`,
+      JSON.stringify(errorResponse),
+      this.constructor.name,
+    );
+
+    res.status(status).json(errorResponse);
   }
 }
