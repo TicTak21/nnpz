bdf45e7e1b2d37a224a79ace54747e6a045f332d
Alex <tictak211@gmail.com>
2021-10-12 19:22:21 +0300

    refactor(server): remove unused filter #4

--- apps/server/src/app/shared/filters/index.ts
+++ apps/server/src/app/shared/filters/index.ts
@@ -1,9 +1,7 @@
 import { Provider } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
-import { GqlExceptionFilter } from './gql/gql-exception.filter';
 import { HttpExceptionFilter } from './http/http-exception.filter';
 
 export const globalFilters: Provider[] = [
-  { provide: APP_FILTER, useClass: GqlExceptionFilter },
   { provide: APP_FILTER, useClass: HttpExceptionFilter },
 ];
