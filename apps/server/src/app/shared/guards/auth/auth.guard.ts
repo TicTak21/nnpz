d8b5ca367d51aed71c8ce29d935163085e06f464
Alex <tictak211@gmail.com>
2021-10-17 14:18:23 +0300

    feat(server): combine gql and http into single guard

---
+++ apps/server/src/app/shared/guards/auth/auth.guard.ts
@@ -0,0 +1,94 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
+import { Request } from 'express';
+import { Observable, of, switchMap } from 'rxjs';
+import { IS_PUBLIC_KEY } from '../../../modules/auth/authentication/decorators';
+import { ETokenType } from '../../../modules/auth/authentication/enums';
+import { AuthenticationService } from '../../../modules/auth/authentication/services';
+import {
+  AuthHeaderNotFoundException,
+  AuthTypeBearerRequired,
+} from '../../../modules/error/exceptions';
+
+type TCtxHandler = (ctx: ExecutionContext) => Observable<boolean>;
+type TCtxType = Partial<Record<GqlContextType, TCtxHandler>> & {
+  default: TCtxHandler;
+};
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly authService: AuthenticationService,
+  ) {}
+
+  private readonly ctxTypeReducer: TCtxType = {
+    http: (ctx: ExecutionContext) => this.handleHttp(ctx),
+    graphql: (ctx: ExecutionContext) => this.handleGql(ctx),
+    default: (ctx: ExecutionContext) => this.handleHttp(ctx),
+  };
+
+  canActivate(ctx: ExecutionContext): Observable<boolean> {
+    const ctxType = ctx.getType<GqlContextType>();
+    const ctxHandler: TCtxHandler =
+      this.ctxTypeReducer[ctxType] || this.ctxTypeReducer['default'];
+
+    return ctxHandler(ctx);
+  }
+
+  private handleHttp(ctx: ExecutionContext): Observable<boolean> {
+    if (this.isPublic(ctx)) return of(true);
+
+    const http = ctx.switchToHttp();
+    const req = http.getRequest<Request>();
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) throw new AuthHeaderNotFoundException();
+
+    const [type, token] = authHeader.split(' ');
+    if (type !== ETokenType.Bearer) throw new AuthTypeBearerRequired();
+
+    return this.validate(token);
+  }
+
+  private handleGql(ctx: ExecutionContext): Observable<boolean> {
+    if (this.isPublic(ctx)) return of(true);
+
+    const gql = GqlExecutionContext.create(ctx).getContext();
+    const req = gql.req;
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) throw new AuthHeaderNotFoundException();
+
+    const [type, token] = authHeader.split(' ');
+    if (type !== ETokenType.Bearer) throw new AuthTypeBearerRequired();
+
+    return this.validate(token);
+  }
+
+  private validate(token: string) {
+    return this.authService.validateToken(token).pipe(
+      switchMap(validationResult => {
+        if (!validationResult)
+          throw new UnauthorizedException(validationResult);
+
+        return of(true);
+      }),
+    );
+  }
+
+  private isPublic(ctx: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      ctx.getHandler(),
+      ctx.getClass(),
+    ]);
+
+    return isPublic;
+  }
+}
--- apps/server/src/app/shared/guards/index.ts
+++ apps/server/src/app/shared/guards/index.ts
@@ -1,7 +1,7 @@
 import { Provider } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import { HttpAuthGuard } from './http/http-auth.guard';
+import { AuthGuard } from './auth/auth.guard';
 
 export const globalGuards: Provider[] = [
-  { provide: APP_GUARD, useClass: HttpAuthGuard },
+  { provide: APP_GUARD, useClass: AuthGuard },
 ];
