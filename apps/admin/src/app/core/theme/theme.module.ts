d48d246beea51250d8d1c934ddd3eca620eacdf3
Alex <tictak211@gmail.com>
2022-02-18 21:28:52 +0200

    fix(admin): remove service from `providers`

--- apps/admin/src/app/core/theme/theme.module.ts
+++ apps/admin/src/app/core/theme/theme.module.ts
@@ -4,7 +4,6 @@ import { LyIconModule } from '@alyle/ui/icon';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BaseThemeModule } from '@nnpz/ui';
-import { ThemeService } from 'ng2-charts';
 import { ThemeSwitchComponent } from './components/theme-switch/theme-switch.component';
 import { CustomMinimaDark } from './custom/custom-minima-dark.theme';
 import { CustomMinimaLight } from './custom/custom-minima-light.theme';
@@ -15,7 +14,6 @@ import { CustomGlobalVariables } from './custom/custom-variables.theme';
   imports: [CommonModule, BaseThemeModule, LyIconModule, LyButtonModule],
   exports: [ThemeSwitchComponent],
   providers: [
-    ThemeService,
     { provide: LY_THEME, useClass: CustomMinimaLight, multi: true },
     { provide: LY_THEME, useClass: CustomMinimaDark, multi: true },
     { provide: LY_THEME_GLOBAL_VARIABLES, useClass: CustomGlobalVariables },
