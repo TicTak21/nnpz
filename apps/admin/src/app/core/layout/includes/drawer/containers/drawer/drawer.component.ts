da750543eedb5ec0537ec0de10b6d30c89c86269
Alex <tictak211@gmail.com>
2022-02-27 22:37:41 +0200

    fix(admin): change subscribition strategy to fix drawer animation

--- apps/admin/src/app/core/layout/includes/drawer/containers/drawer/drawer.component.ts
+++ apps/admin/src/app/core/layout/includes/drawer/containers/drawer/drawer.component.ts
@@ -1,12 +1,18 @@
 import { LyTheme2 } from '@alyle/ui';
 import { LyDrawer } from '@alyle/ui/drawer';
-import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable, tap } from 'rxjs';
+import { Observable, Subscription, tap } from 'rxjs';
 import * as layoutSelectors from '../../../../store/selectors';
 
 const styles = {
-  drawerContainer: {
+  container: {
     minHeight: '100vh',
     height: 'auto',
     transform: 'translate3d(0, 0, 0)',
@@ -18,15 +24,30 @@ const styles = {
   templateUrl: './drawer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DrawerComponent {
+export class DrawerComponent implements OnInit, OnDestroy {
   @ViewChild('drawer') drawer!: LyDrawer;
 
   classes = this.theme.addStyleSheet(styles);
-  opened$: Observable<boolean> = new Observable<boolean>();
+  // TODO: HACK:
+  // since ngIf removes node from DOM we have a bad animation after drawer added back(e.g. while opening)
+  // to prevent this we could either use a default value in component/template
+  // or manually subscribe(without async pipe) to the value
+  // => Is it possibly to use ngIf + async pipe and do not remove node from DOM?
+  opened = true;
+  private opened$: Observable<boolean> = new Observable<boolean>();
+  private openedSub: Subscription = new Subscription();
 
   constructor(private readonly theme: LyTheme2, private readonly store: Store) {
     this.opened$ = this.store
       .select(layoutSelectors.selectDrawerState)
       .pipe(tap(() => this.drawer?.toggle()));
+  }
+
+  ngOnInit() {
+    this.openedSub = this.opened$.subscribe(opened => (this.opened = opened));
+  }
+
+  ngOnDestroy() {
+    this.openedSub.unsubscribe();
   }
 }
