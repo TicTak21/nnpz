d1274f36bd5ca1b60ca888c7b0b1f3bbbac6a1f7
Alex <tictak211@gmail.com>
2022-03-04 12:42:10 +0200

    refactor(admin): extract quill to the editor module

--- apps/admin/src/app/core/core.module.ts
+++ apps/admin/src/app/core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { QuillModule } from 'ngx-quill';
+import { EditorModule } from './editor/editor.module';
 import { LayoutComponent } from './layout/containers/layout/layout.component';
 import { LayoutModule } from './layout/layout.module';
 import { StoreModule } from './store/store.module';
 import { ThemeModule } from './theme/theme.module';
 
 @NgModule({
-  imports: [StoreModule, ThemeModule, LayoutModule, QuillModule.forRoot()],
+  imports: [StoreModule, ThemeModule, LayoutModule, EditorModule],
   exports: [LayoutComponent],
 })
 export class CoreModule {}
---
+++ apps/admin/src/app/core/editor/editor.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { QuillModule } from 'ngx-quill';
+
+@NgModule({
+  imports: [QuillModule.forRoot()],
+})
+export class EditorModule {}
