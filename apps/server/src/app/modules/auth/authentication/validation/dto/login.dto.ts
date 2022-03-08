c6d6db3c666f5bf901b28f34fe5f2ea49ffa9d50
Alex <tictak211@gmail.com>
2022-03-08 21:46:09 +0200

    feat(server): use ILoginDto in server dto

--- apps/server/src/app/modules/auth/authentication/validation/dto/login.dto.ts
+++ apps/server/src/app/modules/auth/authentication/validation/dto/login.dto.ts
@@ -1,8 +1,9 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { ILoginDto } from '@nnpz/types';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
-export class LoginDto {
+export class LoginDto implements ILoginDto {
   @Field()
   @IsNotEmpty()
   @IsEmail()
