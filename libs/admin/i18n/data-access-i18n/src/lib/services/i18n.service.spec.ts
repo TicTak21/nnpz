cca9243596eee6ee67f286824101f1e4770adff4
Alex <tictak211@gmail.com>
2022-04-22 16:50:38 +0300

    test(libs/admin): fix i18n service smoke

--- libs/admin/i18n/data-access-i18n/src/lib/services/i18n.service.spec.ts
+++ libs/admin/i18n/data-access-i18n/src/lib/services/i18n.service.spec.ts
@@ -1,13 +1,12 @@
-import { LyTheme2 } from '@alyle/ui';
+import { TranslocoService } from '@ngneat/transloco';
 import { I18nService } from './i18n.service';
 
 describe('I18nService', () => {
   let service: I18nService;
-  let theme: LyTheme2;
+  let translocoService: TranslocoService;
 
   beforeEach(() => {
-    theme = {} as LyTheme2;
-    service = new I18nService(theme);
+    service = new I18nService(translocoService);
   });
 
   it('should create', () => {
