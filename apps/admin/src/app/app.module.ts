0a40a0e19f3c2a6da4fbc0cfa7757f4c2ba4d3da
Alex <tictak211@gmail.com>
2022-04-22 13:18:38 +0300

    refactor(libs/admin): move transloco config to the i18n

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -2,22 +2,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  TranslocoConfig,
-  TranslocoModule,
-  TRANSLOCO_CONFIG,
-} from '@ngneat/transloco';
 import { AdminFeatureAuthModule } from '@nnpz/admin/feature-auth';
 import { AdminFeatureI18nModule } from '@nnpz/admin/feature-i18n';
 import { AdminFeatureLayoutModule } from '@nnpz/admin/feature-layout';
 import { AdminFeatureStoreModule } from '@nnpz/admin/feature-store';
 import { AdminFeatureThemeModule } from '@nnpz/admin/feature-theme';
-import { environment } from '@nnpz/admin/util-env';
-import { ELangs } from '@nnpz/shared/types';
-import { availableLangs } from 'scoped-translations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { translocoLoader } from './transloco.loader';
 
 const FEATURES = [
   AdminFeatureStoreModule,
@@ -25,7 +16,6 @@ const FEATURES = [
   AdminFeatureThemeModule,
   AdminFeatureAuthModule,
   AdminFeatureI18nModule,
-  TranslocoModule,
 ];
 
 const COMMON = [
@@ -38,18 +28,6 @@ const COMMON = [
 @NgModule({
   declarations: [AppComponent],
   imports: [COMMON, FEATURES],
-  providers: [
-    {
-      provide: TRANSLOCO_CONFIG,
-      useValue: {
-        reRenderOnLangChange: true,
-        availableLangs,
-        defaultLang: ELangs.english,
-        prodMode: environment.production,
-      } as TranslocoConfig,
-    },
-    translocoLoader,
-  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- libs/admin/i18n/feature-i18n/src/lib/admin-feature-i18n.module.ts
+++ libs/admin/i18n/feature-i18n/src/lib/admin-feature-i18n.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
+import {
+  TranslocoConfig,
+  TranslocoModule,
+  TRANSLOCO_CONFIG,
+} from '@ngneat/transloco';
 import { AdminDataAccessI18nModule } from '@nnpz/admin/data-access-i18n';
+import { environment } from '@nnpz/admin/util-env';
+import { ELangs } from '@nnpz/shared/types';
+import { availableLangs } from 'scoped-translations';
+import { translocoLoader } from './transloco.loader';
 
 @NgModule({
-  imports: [AdminDataAccessI18nModule],
+  imports: [AdminDataAccessI18nModule, TranslocoModule],
+  providers: [
+    {
+      provide: TRANSLOCO_CONFIG,
+      useValue: {
+        reRenderOnLangChange: true,
+        availableLangs,
+        defaultLang: ELangs.english,
+        prodMode: environment.production,
+      } as TranslocoConfig,
+    },
+    translocoLoader,
+  ],
 })
 export class AdminFeatureI18nModule {}
