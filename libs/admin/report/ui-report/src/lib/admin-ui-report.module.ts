4c41e40f57f8feee49e223b8d7b1b7489fdffff4
Alex <tictak211@gmail.com>
2022-04-24 16:49:22 +0300

    feat(libs/admin): translate `report` page

--- libs/admin/report/ui-report/src/lib/admin-ui-report.module.ts
+++ libs/admin/report/ui-report/src/lib/admin-ui-report.module.ts
@@ -5,6 +5,8 @@ import { LyTypographyModule } from '@alyle/ui/typography';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AdminUtilI18nModule } from '@nnpz/admin/util-i18n';
+import { scopeLoader } from 'scoped-translations';
 import { AdminUiReportFormComponent } from './report-form/report-form.component';
 import { AdminUiReportTitleComponent } from './report-title/report-title.component';
 
@@ -19,6 +21,12 @@ const COMPONENTS = [AdminUiReportFormComponent, AdminUiReportTitleComponent];
     LyFieldModule,
     LySliderModule,
     LyButtonModule,
+    AdminUtilI18nModule.forChild(
+      'adminUiReport',
+      scopeLoader(
+        (lang: string, root: string) => import(`../${root}/${lang}.json`),
+      ),
+    ),
   ],
   exports: [COMPONENTS],
 })
